d1fb8360b73bded00c188756cfdd043e178bd6c9
Gilmar Santana <g.r.santana@hotmail.com>
2021-03-31 07:15:40 -0300

    Adjusting login.html

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 
+// ---------- Side Menu ---------- //
 var nav_menu = document.getElementById('nav_menu')
 var menu = document.getElementById('menu-img')
 
@@ -10,5 +11,33 @@ menu.onclick = function() {
     nav_menu.style.right = '0px'
   }else {
     nav_menu.style.right = '-151px'
+  }
+}
+
+// ---------- Account ---------- //
+if (document.querySelector('#Account')){
+  var btnLogin = document.querySelector('.t-login');
+  var btnRegister = document.querySelector('.t-register');
+  var boxLogin = document.querySelector('.account-box-login');
+  var boxRegister = document.querySelector('.account-box-register');
+  var indicator = document.getElementById('Indicator');
+
+  boxLogin.style.transition = '1s';
+  boxRegister.style.transition = '1s';
+  indicator.style.transition = '1s';
+  boxLogin.style.left = '0';
+  boxRegister.style.left = '-350px';
+  indicator.style.transform = 'translateX(25px)'
+
+  btnLogin.onclick = function() {
+    boxLogin.style.left = '0';
+    boxRegister.style.left = '-350px';
+    indicator.style.transform = 'translateX(25px)'
+  }
+
+  btnRegister.onclick = function() {
+    boxLogin.style.left = '350px';
+    boxRegister.style.left = '0';
+    indicator.style.transform = 'translateX(165px)'
   }
 }
